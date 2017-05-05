11806a23423752719ac0be530e710dd6d86fea56
ching-k <ching-k@hotmail.com>
2017-05-05 22:10:54 +0800

    start a project

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/*
+$(function () {
+    $('nav a').bind('click', function (event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+});
+*/
+
+
+function sumNumbers(a, b) {
+    return a + b;
+}
+
+/*
+function(){} 基本
++
+$() 初始結構
+=
+$(function(){寫程式碼}) 
+*/
